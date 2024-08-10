956926744e9e2a1c0ef0a1d2424d5c2333d392e0
IrgiBagas <140321137+IrgiBagas@users.noreply.github.com>
2024-08-10 15:28:33 +0700

    update

--- Konversi Suhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
+++ Konversi Suhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
@@ -1,6 +1,13 @@
 package com.komputerkit.konversisuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
+    EditText nilai;
+    TextView hasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,72 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+    }
+    public void load(){
+        spinner = findViewById(R.id.spinner);
+        nilai = findViewById(R.id.nilai);
+        hasil = findViewById(R.id.hasil);
+    }
+    /*
+    public void isiSpinner(){
+        String[] isi = {"Celcius to Reamur, Celcius to Fahrenheit, Celcius to Kelvin"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+        spinner.setAdapter(adapter);
+    }*/
+
+    public void konversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+
+        if(nilai.getText().toString().equals("")){
+            Toast.makeText(this, "Nilai Kosong", Toast.LENGTH_SHORT).show();
+        }else {
+            if (pilihan.equals("Celcius to Reamur")) {
+                CtoR();
+            }
+            if (pilihan.equals("Celcius to Fahrenheit")) {
+                CtoF();
+            }
+            if (pilihan.equals("Celcius to Kelvin")) {
+                CtoK();
+            }
+            if (pilihan.equals("Reamur to Celcius")) {
+                RtoC();
+            }
+            if (pilihan.equals("Reamur to Fahrenheit")) {
+                RtoF();
+            }
+        }
+    }
+
+    public void CtoR(){
+        double suhu = Double.parseDouble(nilai.getText().toString());
+        double total = (4.0/5.0) * suhu;
+
+        hasil.setText(total+"");
+    }
+    public void CtoF(){
+        double suhu = Double.parseDouble(nilai.getText().toString());
+        double total = (9.0/5.0) * suhu + 32;
+
+        hasil.setText(total+"");
+    }
+    public void CtoK(){
+        double suhu = Double.parseDouble(nilai.getText().toString());
+        double total = 273.15 + suhu;
+
+        hasil.setText(total+"");
+    }
+    public void RtoC(){
+        double suhu = Double.parseDouble(nilai.getText().toString());
+        double total = (5.0/4.0) * suhu;
+
+        hasil.setText(total+"");
+    }
+    public void RtoF(){
+        double suhu = Double.parseDouble(nilai.getText().toString());
+        double total = (9.0/4.0) * suhu + 32;
+
+        hasil.setText(total+"");
     }
 }
