998bad3c107ec5cd60462a38ad62e66a31d08a3f
IrgiBagas <140321137+IrgiBagas@users.noreply.github.com>
2024-08-12 12:11:07 +0700

    update

--- SharedPreferences/app/src/main/java/com/komputerkit/sharedpreferences/MainActivity.java
+++ SharedPreferences/app/src/main/java/com/komputerkit/sharedpreferences/MainActivity.java
@@ -1,6 +1,10 @@
 package com.komputerkit.sharedpreferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView etBarang,etStok;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+    }
+    public void load(){
+        etBarang = findViewById(R.id.etBarang);
+        etStok = findViewById(R.id.etStok);
+        sharedPreferences = getSharedPreferences("BARANG",MODE_PRIVATE);
+    }
+
+    public void isisharedPreferences(String barang, float stok){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("barang",barang);
+        editor.putFloat("stok",stok);
+        editor.apply();
+    }
+
+    public void simpan(View view) {
+        String barang = etBarang.getText().toString();
+        float stok = Float.parseFloat(etStok.getText().toString());
+
+        if (barang.isEmpty() || stok == 0.0){
+            Toast.makeText(this, "DATA KOSONG", Toast.LENGTH_SHORT).show();
+        }else {
+            isisharedPreferences(barang,stok);
+            Toast.makeText(this, "DATA DISIMPAN", Toast.LENGTH_SHORT).show();
+        }
+        etBarang.setText("");
+        etStok.setText("");
+    }
+
+    public void tampil(View view) {
+        String barang = sharedPreferences.getString("barang","");
+        float stok = sharedPreferences.getFloat("stok",0);
+
+        etBarang.setText(barang);
+        etStok.setText(stok+"");
     }
 }
